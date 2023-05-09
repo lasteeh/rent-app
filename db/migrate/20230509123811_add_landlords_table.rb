45777c99d0fdc11c4c578b99bf90afd1bc126805
Last David Lantican <lastdavid.lantican@gmail.com>
2023-05-09 21:09:06 +0800

    add landlords table

---
+++ db/migrate/20230509123811_add_landlords_table.rb
@@ -0,0 +1,12 @@
+class AddLandlordsTable < ActiveRecord::Migration[7.0]
+  def change
+    create_table :landlords, id: :uuid do |t|
+      t.string :first_name, null: false
+      t.string :last_name, null: false
+      t.string :email, null: false
+      t.string :phone_number, null: false
+
+      t.timestamps
+    end
+  end
+end
--- spec/factories/landlords.rb
+++ spec/factories/landlords.rb
@@ -1,9 +1,9 @@
 FactoryBot.define do
   factory :landlord do
     # Add appropriate attributes for the landlord model
-
     first_name { 'first name' }
     last_name { 'last name' }
     email { 'landlord1@email.com' }
+    phone_number { '09454545454' }
   end
 end
