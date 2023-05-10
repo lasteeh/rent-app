3424662a1574f0e5b5f22801a0eb552414be5d5a
Last David Lantican <lastdavid.lantican@gmail.com>
2023-05-10 13:49:28 +0800

    ! modify landlord_spec.rb and landlord.rb model

--- app/models/landlord.rb
+++ app/models/landlord.rb
@@ -1,2 +1,26 @@
 class Landlord < ApplicationRecord
+  # validations
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :email,
+            presence: true,
+            uniqueness: {
+              case_sensitive: false,
+            },
+            format: {
+              with: URI::Mailto::EMAIL_REGEXP,
+            }
+  validates :phone_number, presence: true, uniqueness: { case_sensitive: false }
+
+  # callbacks
+  before_save :normalize_email_address
+
+  # custom methods
+
+  # private methods
+  private
+
+  def normalize_email_address
+    self.email = email.downcase
+  end
 end
--- spec/models/landlord_spec.rb
+++ spec/models/landlord_spec.rb
@@ -1,8 +1,27 @@
 require 'rails_helper'
 
 RSpec.describe Landlord, type: :model do
-  it 'creates a valid landlord' do
-    landlord = create(:landlord)
-    expect(landlord).to be_valid
+  let(:landlord) { FactoryBot.create(:landlord) }
+
+  # validations
+  describe 'validations' do
+    subject { FactoryBot.create(:landlord) }
+
+    it { should validate_presence_of(:first_name) }
+    it { should validate_presence_of(:last_name) }
+    it { should validate_presence_of(:email) }
+    it { should validate_uniqueness_of(:email).case_insensitive }
+    it { should_not allow_value('hakdog.email.com').for(:email) }
+    it { should validate_presence_of(:phone_number) }
+    it { should validate_uniqueness_of(:phone_number).case_insensitive }
+  end
+
+  # callbacks
+  describe 'callbacks' do
+    it 'normalizes the email address before saving' do
+      landlord.email = 'HAKDOG@EMail.com'
+      landlord.save
+      expect(landlord.reload.email).to eq('hakdog@email.com')
+    end
   end
 end
