a2afdb78204a8023899f5c279bdcdcb20ebf08c6
Last David Lantican <lastdavid.lantican@gmail.com>
2023-05-13 12:45:19 +0800

    add register method in property model

--- app/models/property.rb
+++ app/models/property.rb
@@ -1,8 +1,22 @@
 class Property < ApplicationRecord
+  # associations
   belongs_to :landlord
 
+  # validations
   validates :address, presence: true
   validates :city, presence: true
   validates :province, presence: true
   validates :zip_code, presence: true
+
+  # registration
+  def self.register(property_params)
+    property = new(property_params)
+
+    if property.save
+      return property, nil
+    else
+      error_messages = property.errors.full_messages
+      return property, error_messages
+    end
+  end
 end
