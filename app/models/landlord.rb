48d3547056af1ad989cfe3616c35871853d13052
Last David Lantican <lastdavid.lantican@gmail.com>
2023-05-13 11:43:14 +0800

    add has many property association in landlord model

--- app/models/landlord.rb
+++ app/models/landlord.rb
@@ -1,4 +1,7 @@
 class Landlord < ApplicationRecord
   include UserRegistration # registration for user
   include UserAuthentication # authentication for user
+
+  # associations
+  has_many :property, dependent: :destroy
 end
