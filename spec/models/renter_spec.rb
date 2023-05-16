f4a91c24844c0f2af12be5ec125448d4762535fb
Last David Lantican <lastdavid.lantican@gmail.com>
2023-05-16 19:39:47 +0800

    + add password tests for renter

--- spec/models/renter_spec.rb
+++ spec/models/renter_spec.rb
@@ -11,18 +11,28 @@ RSpec.describe Renter, type: :model do
     it { should allow_value('hak dog').for(:first_name) }
     it { should_not allow_value('hak !!! dog').for(:first_name) }
     it { should_not allow_value(' - ').for(:first_name) }
+
     it { should validate_presence_of(:last_name) }
     it { should allow_value('sam-smith').for(:last_name) }
+
     it { should validate_presence_of(:email) }
     it { should validate_uniqueness_of(:email).case_insensitive }
     it { should_not allow_value('hakdog.email.com').for(:email) }
+
     it { should validate_presence_of(:phone_number) }
     it { should validate_uniqueness_of(:phone_number).case_insensitive }
+
+    it { should_not allow_value('1234567').for(:password) }
+    it { should_not allow_value('12345678').for(:password) }
+    it { should_not allow_value('12345678a').for(:password) }
+    it { should_not allow_value('12345678aA').for(:password) }
+    it { should allow_value('12345678aA.').for(:password) }
+
+    it 'should validate matching password and password confirmation' do
+      renter.password = 'a1b2c3DDDD!'
+      renter.password_confirmation = 'a1b2c3DDDD!!'
+      expect(renter).to_not be_valid
+    end
   end
 
-  it 'should validate matching password and password confirmation' do
-    renter.password = 'a1b2c3DDDD!'
-    renter.password_confirmation = 'a1b2c3DDDD!!'
-    expect(renter).to_not be_valid
-  end
 end
